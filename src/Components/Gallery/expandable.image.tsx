65ab8e75e9fc8376ad5c63a01f912ba0dfd1d12f
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-12-11 11:34:21 +0530

    Fix styling.

--- src/Components/Gallery/expandable.image.tsx
+++ src/Components/Gallery/expandable.image.tsx
@@ -4,6 +4,7 @@ import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import { ExpandLess, ExpandMore, ZoomIn, ZoomOut } from '@mui/icons-material';
 
 interface ExpandableImageProps {
   src: string;
@@ -19,26 +20,28 @@ const ExpandableImage: React.FC<ExpandableImageProps> = ({ src, alt, style }) =>
   };
 
   return (
-    <Card style={{width: isExpanded? '100%' :'50%'}}>
-      <CardMedia
-        component="img"
-        image={src}
-        alt={alt || 'Expandable image'}
-        onClick={toggleExpand}
-        style={{ cursor: 'pointer',width:'100%', padding:'10px' }}
-      />
+    <div style={{ position: 'relative', width: isExpanded ? '100%' : '50%' }}>
+        <Card >
+        <CardMedia
+            component="img"
+            image={src}
+            alt={alt || 'Expandable image'}
+            onClick={toggleExpand}
+            style={{ cursor: 'pointer',width:'100%', padding:'10px' }}
+        />
 
-      {isExpanded && (
-        <IconButton onClick={toggleExpand}>
-          <CloseIcon />
-        </IconButton>
-      )}
-      {!isExpanded && (
-        <IconButton onClick={toggleExpand}>
-          <ZoomInIcon />
-        </IconButton>
-      )}
-    </Card>
+        {isExpanded && (
+            <IconButton onClick={toggleExpand}  style={{ position: 'absolute', right: 0, top: 0 }}>
+            <ZoomOut />
+            </IconButton>
+        )}
+        {!isExpanded && (
+            <IconButton onClick={toggleExpand} style={{ position: 'absolute', right: 0, top: 0 }}>
+            <ZoomIn />
+            </IconButton>
+        )}
+        </Card>
+    </div>
   );
 };
 
